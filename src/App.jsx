6ec10f216cdc83261c0730c87ef1d3a7376cbcd4
Rahimjon <abduraximovrahimjon8@gmail.com>
2024-06-23 11:06:15 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' grid place-content-center h-screen '>
+      THAT IS KOKITO TEAM'S INITIAL VIEW. <br />
+      coming soon ...
+    </div>
+  )
+}
+
+export default App
